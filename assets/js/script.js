b4895bd2646a11f293aa34136b8a378dd829e6b5
tinyflow-dev <tinyflow.dev@tinyflow.agency>
2026-06-25 03:18:42 +0600

    LAOS-Frontend init

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Team Member Slider Functions
+    new Swiper('[data-swiper-element-id="team"]', {
+        loop: true,
+        autoplay: {
+            delay: 3000,
+        },
+        slidesPerView: 1,
+        spaceBetween: 20,
+        breakpoints: {
+            600: {
+                slidesPerView: 2,
+            },
+            992: {
+                slidesPerView: 3,
+            },
+        },
+        navigation: {
+            nextEl: '[data-swiper-navigation-id="team-next"]',
+            prevEl: '[data-swiper-navigation-id="team-prev"]',
+        },
+    });
+});
